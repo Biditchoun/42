3e8371c4aef3ea0e9c96d093c6e4f89d1f1062ba
Biditchoun <swijnber@student.s19.be>
2022-05-05 17:54:49 +0200

    Added a test for strncmp

--- Common tree/LibftTests.c
+++ Common tree/LibftTests.c
@@ -6,7 +6,7 @@
 /*   By: swijnber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 22:59:50 by swijnber          #+#    #+#             */
-/*   Updated: 2022/05/04 18:15:55 by swijnber         ###   ########.fr       */
+/*   Updated: 2022/05/05 17:51:10 by swijnber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -724,6 +724,14 @@ int	main()
 	if (strncmp (b0, b1, 13) != ft_strncmp(b0, b1, 13) && ft_strncmp(b0, b1, 13) != 1)
 	{
 		printf (" KO\nParams : s0[0] = '\\200', s0, 13\nstrncmp = %i, ft_strncmp = %i\n", strncmp(b0, b1, 13), ft_strncmp(b0, b1, 13));
+		return (1);
+	}
+	bzero (b0, 2);
+	strcpy(b0, "\x12\xff\x65\x12\xbd\xde\xad");
+	strcpy(b1, "\x12\x02");
+	if (strncmp (b0, b1, 13) != ft_strncmp(b0, b1, 13) && ft_strncmp(b0, b1, 13) != 1)
+	{
+		printf (" KO\nParams : \"\\x12\\xff\\x65\\x12\\xbd\\xde\\xad\", \"\\x12\\x02\", 13\nstrncmp = %i, ft_strncmp = %i\n", strncmp(b0, b1, 13), ft_strncmp(b0, b1, 13));
 		return (1);
 	}
 	bzero (b0, 2);
