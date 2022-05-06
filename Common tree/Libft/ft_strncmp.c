4cd94b5745312ddc201828d519a7162bd184d271
Biditchoun <swijnber@student.s19.be>
2022-05-06 14:21:52 +0200

    Using unsigned char instead of char, resulting in a much shorter function

--- Common tree/Libft/ft_strncmp.c
+++ Common tree/Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: swijnber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 01:00:00 by swijnber          #+#    #+#             */
-/*   Updated: 2022/05/05 17:50:43 by swijnber         ###   ########.fr       */
+/*   Updated: 2022/05/06 14:13:47 by swijnber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
 
-	i = 0;
-	if (!n)
-		return (0);
-	while (s1 && s2 && i < n - 1)
-	{
-		if (s1[i] == s2[i])
-			i++;
-		else
+	i = -1;
+	while (s1 && s2 && ++i < n - 1)
+		if (s1[i] != s2[i])
 			break ;
-	}
-	if (s1[i] != s2[i])
-	{
-		if ((s1[i] == '\200' && !s2[i]) || (!s1[i] && s2[i] == '\200'))
-			return (s2[i] - s1[i]);
-		return (s1[i] - s2[i]);
-	}
+	if (s1[i] != s2[i] && n)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
