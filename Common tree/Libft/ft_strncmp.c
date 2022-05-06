71c2657330331977053bf52a1b313be38c7d0ae5
Biditchoun <swijnber@student.s19.be>
2022-05-06 14:40:46 +0200

    Stopping at \0

--- Common tree/Libft/ft_strncmp.c
+++ Common tree/Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: swijnber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 01:00:00 by swijnber          #+#    #+#             */
-/*   Updated: 2022/05/06 14:13:47 by swijnber         ###   ########.fr       */
+/*   Updated: 2022/05/06 14:39:02 by swijnber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	i = -1;
 	while (s1 && s2 && ++i < n - 1)
-		if (s1[i] != s2[i])
+		if (s1[i] != s2[i] /*|| !s1[i] || !s2[i]*/)
 			break ;
 	if (s1[i] != s2[i] && n)
 		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
